5e8a1942dfef5caace04a117267966ed955ef172
lp-0406 <laxmiprashasthi@gmail.com>
2025-11-06 16:34:38 +0530

    Add files via upload

---
+++ day-01/idc day-01.sql
@@ -0,0 +1,16 @@
+use hospital;
+#day-01
+# task - 01
+#select * from patients;
+
+#task - 02
+#select patient_id, name, age from patients;
+
+
+#task - 03
+select * from services_weekly
+limit 10;
+
+
+#task - 04
+select distinct service from services_weekly;
